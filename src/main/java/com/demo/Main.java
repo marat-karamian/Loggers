8015288a7bb4edb5d768986a264fbecc88bee14f
marat-karamian <marat_karamian@epam.com>
2018-09-13 21:41:46 +0300

    Initial commit

---
+++ src/main/java/com/demo/Main.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SomeClass t = new SomeClass();
+        t.startThread();
+    }
+
+}
---
+++ src/main/java/com/demo/SomeClass.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.logging.Logger;
+
+public class SomeClass {
+
+    private Thread thread = new Thread();;
+
+    public void startThread() {
+        Logger log = Logger.getLogger(getClass().getName());
+        thread.start();
+        log.info("Message " + Thread.currentThread().getName());
+    }
+}
