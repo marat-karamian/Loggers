13d93c8fb429822addff4bb4a7eadc6aff4949e2
marat-karamian <marat_karamian@epam.com>
2018-09-17 10:39:09 +0300

    "SomeClass" class deleted. Logic moved to Main class

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -1,11 +1,28 @@
 package com.demo;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        SomeClass t = new SomeClass();
-        t.startThread();
+        System.out.println("Text");
+        Logger log = Logger.getLogger(Main.class.getName());
+        Thread thread = new Thread();
+        thread.start();
+        log.info("Info has started from thread " + Thread.currentThread().getName());
+        log.warning("Warning has started from thread " + Thread.currentThread().getName());
+
+        Handler logHandler = new ConsoleHandler();
+        logHandler.setLevel(Level.FINE);
+        log.setLevel(Level.FINEST);
+        log.addHandler(logHandler);
+
+        log.fine("Fine has started from thread " + Thread.currentThread().getName());
+
     }
 
 }
