fd300e0829ab306c7611c5da2854b32f85d93ef8
marat-karamian <marat_karamian@epam.com>
2018-09-19 21:05:57 +0300

    FileHandler added

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -1,11 +1,37 @@
 package com.demo;
 
+import sun.net.www.protocol.http.logging.HttpLogFormatter;
+
+import java.io.IOException;
+import java.util.logging.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        SomeClass t = new SomeClass();
-        t.startThread();
+        System.out.println("Text");
+
+        Logger log = Logger.getAnonymousLogger();
+
+        try {
+            FileHandler handler = new FileHandler("julLog.html");
+            handler.setFormatter(new HttpLogFormatter());
+            log.addHandler(handler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+            log.info("Info has started");
+            log.warning("Warning has started");
+
+            Handler logHandler = new ConsoleHandler();
+            logHandler.setLevel(Level.FINEST);
+            log.setLevel(Level.FINE);
+            log.addHandler(logHandler);
+
+            log.fine("Fine has started");
+
+
+        }
+
     }
-
-}
